8773184e840d7fdfe10c804de0348d2b18fca36d
hmp-anthony <anthonygoddard962@gmail.com>
2024-09-25 12:58:50 +0100

    map_to_range implementation started

---
+++ include/asiant/align.hpp
@@ -0,0 +1,53 @@
+#ifndef ALIGN_HPP
+#define ALIGN_HPP
+
+#include <asiant/kinematic.hpp>
+
+namespace asiant {
+    class align {
+    public:
+        void set_character(kinematic& c) {
+            character = c;
+        }
+        void set_target(kinematic& t) {
+            target = t;
+        }
+        void set_max_angular_acceleration(real a) {
+            max_angular_acceleration = a;
+        }
+        void set_max_rotation(real r) {
+            max_rotation = r;
+        }
+        void set_target_radius(real r) {
+            target_radius = r;
+        }
+        void set_slow_radius(real r) {
+            slow_radius = r;
+        }
+        void set_time_to_target(real t) {
+            time_to_target = t;
+        }
+        kinematic& get_character() {
+            return character;
+        }
+        kinematic& get_target() {
+            return target;
+        }
+        
+        void update() {
+            rotation = target.get_orientation() - character.get_orientation();
+            // perfect example of why we need to test!
+            rotation = map_to_range(rotation);
+        }
+    private:
+        kinematic character;
+        kinematic target;
+        real max_angular_acceleration;
+        real max_rotation;
+        real target_radius;
+        real slow_radius;
+        real time_to_target;
+    };
+};
+
+#endif
