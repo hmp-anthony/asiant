0cd7edd51bf48eaf51fe9c306250fbae903c249c
hmp-anthony <anthonygoddard962@gmail.com>
2024-09-29 11:12:38 +0100

    added header files

--- include/asiant/steering.hpp
+++ include/asiant/steering.hpp
@@ -4,11 +4,13 @@
 #include <asiant/precision.hpp>
 #include <asiant/vector.hpp>
 
+#include <memory>
+
 namespace asiant {
     class steering {
     public:
         steering() {
-            linear = vector(0, 0, 0);
+            linear = vector(0,0,0);
             angular = 0;
         }
         vector get_linear() const {
@@ -23,11 +25,35 @@ namespace asiant {
         void set_angular(const real r) {
             angular = r;
         }
-
     private:
         vector linear;
         real   angular;
     };
+
+    class steering_behaviour {
+    public:
+        std::shared_ptr<kinematic> get_character() {
+            return character;
+        }
+        virtual std::shared_ptr<steering> get_steering() = 0;
+    protected:
+        std::shared_ptr<kinematic> character;
+    };
+
+    class seek : public steering_behaviour {
+    public:
+        std::shared_ptr<vector> get_target() {
+            return target;
+        }
+        virtual std::shared_ptr<steering> get_steering();
+    protected:
+        std::shared_ptr<const vector> target;
+    }
+
+    class flee : public seek {
+    public:
+        virtual std::shared_ptr<steering> get_steering();
+    }
 };
 
 #endif
