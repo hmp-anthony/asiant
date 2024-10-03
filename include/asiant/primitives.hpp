ef46aca613d92137388b96940cd172e5b51e6f45
hmp-anthony <anthonygoddard962@gmail.com>
2024-10-03 21:01:53 +0100

    added sphere primitive

---
+++ include/asiant/primitives.hpp
@@ -0,0 +1,13 @@
+#ifndef PRIMITIVES_HPP
+#define PRIMITIVES_HPP
+
+#include <asiant/vector.hpp>
+
+namespace asiant{
+    struct sphere {
+        vector position_;
+        real radius_;
+    };
+};
+
+#endif
