9ea719af0f5b80255dee7aa23294f75ef933f35b
hmp-anthony <anthonygoddard962@gmail.com>
2024-10-07 20:42:00 +0100

    implementation of pipeline demo complete

--- demos/pipeline/pipeline.cpp
+++ demos/pipeline/pipeline.cpp
@@ -11,20 +11,20 @@ using namespace graphics_utils;
 using namespace asiant;
 
 namespace {
-    constexpr int number_of_obstacles = 100;
+    constexpr int number_of_obstacles = 200;
 }
 
 class pipeline_demo : public application {
 public:
     pipeline_demo();
 
-    //virtual bool key(unsigned char key);
+    virtual bool key(unsigned char key);
     virtual void update();
     virtual void display();
-    //virtual const char* get_title();
-    //virtual void get_help_text(const char*** lines, unsigned *line_count);
-    //virtual unsigned get_status_count();
-    //virtual const char* get_status_text(unsigned slot);
+    virtual const char* get_title();
+    virtual void get_help_text(const char*** lines, unsigned *line_count);
+    virtual unsigned get_status_count();
+    virtual const char* get_status_text(unsigned slot);
     virtual void create_random_goal();
 private:
     std::shared_ptr<kinematic> kinematic_;
@@ -86,6 +86,30 @@ pipeline_demo::pipeline_demo() : application(), auto_new_goal(true) {
     create_random_goal();
 }
 
+static const char *default_help[] = {
+    "asiant: Steering Pipeline Demo",
+    "H - Toggle help.",
+	"",
+	"A - Automatically move the goal",
+	"N - Choose a new goal"
+};
+
+bool pipeline_demo::key(unsigned char key) {
+    if (application::key(key)) return true;
+
+    switch(key) {
+	case 'a': case 'A':
+		auto_new_goal = !auto_new_goal;
+		return true;
+
+	case 'n': case 'N':
+		create_random_goal();
+		return true;
+    }
+
+    return false;
+}
+
 void pipeline_demo::update() {
     float duration = timer::get().last_frame_duration * 0.005f;
     auto steer = std::make_shared<steering>();
@@ -140,6 +164,48 @@ void pipeline_demo::display() {
         circle(s->center_[0], s->center_[1], s->radius_, 20);
         glPopMatrix();
 	}
+
+    display_help();
+}
+
+const char* pipeline_demo::get_title()
+{
+    return default_help[0];
+}
+
+void pipeline_demo::get_help_text(const char*** lines,
+                                  unsigned *line_count)
+{
+    *lines = default_help;
+    *line_count = sizeof(default_help) / sizeof(const char*);
+}
+
+unsigned pipeline_demo::get_status_count()
+{
+    return 1;
+}
+
+const char* pipeline_demo::get_status_text(unsigned slot)
+{
+    
+    bool is_goal = false;
+    if(steering_pipe_->path_ && targeter_->goal_.position_) {
+        is_goal = 
+            steering_pipe_->path_->goal_.position_.value() == targeter_->goal_.position_.value();
+    }
+	if (constraint_->suggestion_used_) 
+	{
+		return "Honouring Constraint";
+	}
+	else if (is_goal)
+	{
+		return "Heading for goal";
+	}
+	else
+	{
+		return "Wandering";
+	}
+    
 }
 
 void pipeline_demo::create_random_goal() {
