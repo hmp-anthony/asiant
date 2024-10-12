8adb782983363e025a3c558271be3bea951ba560
hmp-anthony <anthonygoddard962@gmail.com>
2024-10-12 10:54:55 +0100

    compiling

--- tests/priority_queue_test.cpp
+++ tests/priority_queue_test.cpp
@@ -17,7 +17,7 @@ TEST(priority_queue, push) {
     auto nr_1 = std::make_shared<example_node_record>();
     nr_1->node_index_ = 1;
     nr_1->connection_ = connection(1, 3);
-    nr_1->cost_so_far_ = 0.1;
+    nr_1->cost_so_far_ = 0.9;
 
     auto nr_2 = std::make_shared<example_node_record>();
     nr_2->node_index_ = 9;
@@ -34,7 +34,7 @@ TEST(priority_queue, push) {
     pq.push(nr_2);
     pq.push(nr_3);
 
-    auto top = pq.top();
+    auto top = static_pointer_cast<example_node_record>(pq.top());
     ASSERT_NEAR(top->cost_so_far_, 0.1, 0.0001); 
 }
 
