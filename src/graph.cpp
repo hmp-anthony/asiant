3bb155ff837bd0dd7133371f9ac33f4bfb60237b
hmp-anthony <anthonygoddard962@gmail.com>
2024-10-15 14:50:45 +0100

    removed redun code

--- src/graph.cpp
+++ src/graph.cpp
@@ -30,59 +30,8 @@ namespace asiant {
         return connections_[from_node];
     }
 
-/*    void dijkstra(graph g, int start, int goal) {
-        auto start_record = std::make_shared<node_record>();
-        start_record->node_ = node(start, nullptr);
-        start_record->cost_so_far_ = 0;
 
-        auto open = priority_queue();
-        auto closed = priority_queue();
-        open.push(start_record);
-        
-        auto current = std::make_shared<node_record>();
-
-        while(open.size() > 0) {
-            auto previous_current = current;
-            current = open.top();
-            current->previous_ = previous_current;
-            std::cout << "current " << current->node_.get_value() << std::endl;
-            std::cout << "current cost " << current->cost_so_far_ << std::endl;
-            if(current->node_.get_value() == goal) {
-                break;
-            }
-            auto connections = g.get_connections(current->node_.get_value());
-            for(auto& connection : connections) {
-                auto end_node = connection->get_to();
-                auto end_node_cost = current->cost_so_far_ + connection->get_cost();
-                auto end_node_record = std::make_shared<node_record>();
-                if(closed.contains(end_node)) {
-                    continue;
-                } else if(open.contains(end_node)) {
-                    auto end_node_record = open.find(end_node);
-                    if(end_node_record->cost_so_far_ <= end_node_cost) continue;
-                } else {
-                    end_node_record->node_ = node(end_node, nullptr);
-                }
-                end_node_record->cost_so_far_ = end_node_cost;
-                end_node_record->connection_ = *connection;
-                if(!open.contains(end_node)) {
-                    open.push(end_node_record);
-                }
-            }
-            std::cout << "before pop" << std::endl;
-            std::cout << "current : " << current->node_.get_value() << std::endl;
-            open.print();
-            open.pop();
-            std::cout << "after pop" << std::endl;
-            closed.push(current);
-        }
-        while(current != nullptr) {
-            std::cout << current->node_.get_value() << std::endl;;
-            current = current->previous_;
-        }
-    }
-};*/
-std::shared_ptr<node_record> dijkstra(graph g, int start, int goal) {
+    std::shared_ptr<node_record> dijkstra(graph g, int start, int goal) {
         auto record = std::make_shared<node_record>();
         record->node_ = node(start, nullptr);
         record->cost_so_far_ = 0;
