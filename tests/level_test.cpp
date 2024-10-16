42e376fbc5901b814e1fc4b1fbb4fca928ea3c46
hmp-anthony <anthonygoddard962@gmail.com>
2024-10-16 16:05:17 +0100

    further clean up

--- tests/level_test.cpp
+++ tests/level_test.cpp
@@ -10,9 +10,7 @@ using namespace asiant;
 
 TEST(level, constructor) {
     std::string level_location = "test_levels/level_1.txt";
-    std::filesystem::path cwd = std::filesystem::current_path() / level_location;
-    std::cout << cwd.string() << std::endl;
     auto l = level(level_location, 800, 600);
     l.print();
-    //ASSERT_EQ(1, 1);
+    ASSERT_EQ(1, 1);
 }
